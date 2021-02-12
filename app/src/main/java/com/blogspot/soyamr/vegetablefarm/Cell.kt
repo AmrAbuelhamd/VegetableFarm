f5422456fff8634a4c2c293c976baa5ff4e8f22f
Amr Mokhamed <amr.abuelhamd@gmail.com>
2021-02-13 00:29:50 +0700

    completed basic requirements

--- app/src/main/java/com/blogspot/soyamr/vegetablefarm/Cell.kt
+++ app/src/main/java/com/blogspot/soyamr/vegetablefarm/Cell.kt
@@ -10,28 +10,71 @@ class Cell {
     var job: Job? = null
 
 
+    companion object {
+        var factor = 1F;
+        private const val speed = 500
+    }
+
     fun plant() {
         job?.cancel()
-        state.value = State.Planted
         job = GlobalScope.launch(Dispatchers.Main) {
-            delay(500)
-            state.value = State.Green
-            delay(500)
-            state.value = State.Immature
-            delay(500)
-            state.value = State.Mature
-            delay(500)
-            state.value = State.Overgrow
+            state.value!!.next(this@Cell)
         }
     }
 
+    fun harvest(): Int =
+        when (state.value) {
+            State.Empty -> throw RuntimeException("harvesting empty cell")
+            State.Planted, State.Green -> 0
+            null -> throw RuntimeException("state is null")
+            else -> {
+                job?.cancel()
+                val res = state.value!!.income
+                state.value = State.Empty
+                res
+            }
+        }
 
-    enum class State(val color: Int) {
-        Empty(Color.WHITE),
-        Planted(Color.BLACK),
-        Green(Color.GREEN),
-        Immature(Color.YELLOW),
-        Mature(Color.RED),
-        Overgrow(Color.GRAY);
+
+    enum class State(val color: Int, val income: Int = 0) {
+        Empty(Color.WHITE) {
+            override suspend fun next(cell: Cell) {
+                cell.state.value = Planted
+                cell.state.value!!.next(cell)
+            }
+        },
+        Planted(Color.BLACK) {
+            override suspend fun next(cell: Cell) {
+                delay((speed / factor).toLong())
+                cell.state.value = Green
+                cell.state.value!!.next(cell)
+            }
+        },
+        Green(Color.GREEN) {
+            override suspend fun next(cell: Cell) {
+                delay((speed / factor).toLong())
+                cell.state.value = Immature
+                cell.state.value!!.next(cell)
+            }
+        },
+        Immature(Color.YELLOW, 3) {
+            override suspend fun next(cell: Cell) {
+                delay((speed / factor).toLong())
+                cell.state.value = Mature
+                cell.state.value!!.next(cell)
+            }
+        },
+        Mature(Color.RED, 5) {
+            override suspend fun next(cell: Cell) {
+                delay((speed / factor).toLong())
+                cell.state.value = Overgrow
+            }
+        },
+        Overgrow(Color.GRAY, -1) {
+            override suspend fun next(cell: Cell) {
+            }
+        };
+
+        abstract suspend fun next(cell: Cell);
     }
 }
--- app/src/main/java/com/blogspot/soyamr/vegetablefarm/CellViewModel.kt
+++ app/src/main/java/com/blogspot/soyamr/vegetablefarm/CellViewModel.kt
@@ -1,9 +1,11 @@
 package com.blogspot.soyamr.vegetablefarm
 
 import android.view.View
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.slider.Slider
 
 class CellViewModel : ViewModel() {
 
@@ -12,6 +14,10 @@ class CellViewModel : ViewModel() {
     private val _cellState: MutableLiveData<MutableList<MutableLiveData<Cell.State>>> =
         MutableLiveData(ArrayList(16))
     val cellState: LiveData<MutableList<MutableLiveData<Cell.State>>> = _cellState
+
+    private val _money: MutableLiveData<Int> = MutableLiveData(100)
+    val money: LiveData<Int> = _money
+
 
     init {
         for (i in 0..15) {
@@ -22,6 +28,18 @@ class CellViewModel : ViewModel() {
     }
 
     fun plant(view: View) {
-        cells[view.tag.toString().toInt()].plant()
+        val index = view.tag.toString().toInt();
+        if (cellState.value?.get(index)?.value == Cell.State.Empty) {
+            if (_money.value!! >= 2) {
+                cells[index].plant()
+                _money.value = _money.value?.minus(2)
+            }
+        } else
+            _money.value = _money.value?.plus(cells[index].harvest())
+
+    }
+
+    fun onValueChanged(value: Float) {
+        Cell.factor = value
     }
 }
